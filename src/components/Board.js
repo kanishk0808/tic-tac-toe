e37408e37a9807090eb72fa86774f4c2b250b8b2
Kanishk <kanishk0808@gmail.com>
2020-07-26 00:25:52 +0530

    Initial Commit

---
+++ src/components/Board.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Square from './Square'
+
+const Board = () => {
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default Board
---
+++ src/components/Game.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Game = () => {
+    return (
+        <div>
+            Tic Tac Toe
+        </div>
+    )
+}
+
+export default Game
