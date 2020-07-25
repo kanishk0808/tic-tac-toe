d037a9b7c85d61d80678b6502019c59d13a4d1e0
Kanishk <kanishk0808@gmail.com>
2020-07-26 00:51:50 +0530

    Modified Board.js to add Squares

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,6 +4,15 @@ import Square from './Square'
 const Board = () => {
     return (
         <div>
+            <Square value='1' />
+            <Square value='2' />
+            <Square value='3' />
+            <Square value='4' />
+            <Square value='5' />
+            <Square value='6' />
+            <Square value='7' />
+            <Square value='8' />
+            <Square value='9' />
         </div>
     )
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Board from './Board'
 
 const Game = () => {
     return (
         <div>
-            Tic Tac Toe
+            <Board />
         </div>
     )
 }
