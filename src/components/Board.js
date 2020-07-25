5b6d286eb0f04b014e73866296b67b1ae29e23e1
Kanishk <kanishk0808@gmail.com>
2020-07-26 01:27:05 +0530

    Styles Square.js and Board.js to display Grid

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import Square from './Square'
 
+const styles = {
+    border: '1px solid darkgrey',
+    height: '240px',
+    width: '240px',
+    margin: '0 auto',
+    display: 'grid',
+    gridTemplate: 'repeat(3, 1fr)/repeat(3, 1fr)'
+}
+
 const Board = () => {
     return (
-        <div>
+        <div style={styles}>
             <Square value='1' />
             <Square value='2' />
             <Square value='3' />
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,8 +1,17 @@
 import React from 'react'
 
+const styles = {
+    background: 'white',
+    height: '80px',
+    width: '80px',
+    border: '1px solid darkgrey',
+    fontSize: '50px',
+    outline: 'none'
+}
+
 const Square = ({ value, onClick }) => {
     return (
-        <button onClick={onClick}>
+        <button style={styles} onClick={onClick}>
             {value}
         </button>
     )
