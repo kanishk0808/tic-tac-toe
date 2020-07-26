a4f3ec9b153e8ce8d110a6ddaad14b86dd1ce90b
Kanishk <kanishk0808@gmail.com>
2020-07-26 21:55:32 +0530

    Game Logic Completed

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,11 @@
 import React, { useState } from 'react'
+import { gameLogic } from '../GameLogic'
 import Board from './Board'
+
+const styles = {
+    width: '250px',
+    margin: '3rem auto'
+}
 
 const container = {
     margin: '0 auto',
@@ -11,6 +17,8 @@ const Game = () => {
         player2: 'Player 2'
     })
     const [board, setBoard] = useState(Array(9).fill(null))
+    const [p1IsNext, setP1isNext] = useState(true)
+    const winner = gameLogic(board)
 
     const nameHandler = key => {
         return e => {
@@ -21,18 +29,38 @@ const Game = () => {
         }
     }
 
+    const handleClick = i => {
+        const boardCopy = [...board];
+        if (winner || boardCopy[i]) return
+        boardCopy[i] = p1IsNext ? 'X' : 'O'
+        setBoard(boardCopy)
+        setP1isNext(!p1IsNext)
+    }
+
+    const buttonHandler = () => {
+        return <button onClick={() => setBoard(Array(9).fill(null))}>
+            Reset Game!
+        </button>
+    }
+
     return (
         <>
-            <div style={{ display: 'inline-block', margin: '5rem 16rem' }}>
-                <input style={{ fontSize: '20px', fontWeight: '700', height: '50px', width: '200px', borderRadius: 20, backgroundColor: '#78c28c', textAlign: 'center', borderWidth: '0', outline: 'none' }} value={players.player1} onChange={nameHandler('player1')} />
+            <div style={{ display: 'inline-block', margin: '3rem 16rem' }}>
+                <input style={{ fontSize: '20px', fontWeight: '700', height: '50px', width: '200px', borderRadius: 20, backgroundColor: '#db8c8c', textAlign: 'center', borderWidth: '0', outline: 'none' }} value={players.player1} onChange={nameHandler('player1')} />
             </div>
-            <button style={{ height: '50px', width: '100px', borderRadius: 20 }}>Start</button>
-            <div style={{ display: 'inline-block', margin: '5rem 16rem' }}>
+            <button style={{ height: '50px', width: '100px', borderRadius: 20, outline: 'none' }}>Start</button>
+            <div style={{ display: 'inline-block', margin: '3rem 16rem' }}>
                 <input style={{ fontSize: '20px', fontWeight: '700', height: '50px', width: '200px', borderRadius: 20, backgroundColor: '#db8c8c', textAlign: 'center', borderWidth: '0', outline: 'none' }} value={players.player2} onChange={nameHandler('player2')} />
             </div>
-            <Board squares={board} onClick={() => { }} />
+            <Board squares={board} onClick={handleClick} />
+            <div style={styles}>
+                <p>{(winner === 'X') ? players.player1 + ' Wins' : (winner === 'O') ? players.player2 + ' Wins' : null}</p>
+                {buttonHandler()}
+            </div>
         </>
     )
 }
 
 export default Game
+
+// 78c28c
